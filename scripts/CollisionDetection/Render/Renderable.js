3ddaa1bd9e57ae603630ccb5a3ca385a3cb54b46
beije <git@benjaminhorn.se>
2013-12-18 12:31:19 +0100

    Made renderable so it actually renders something.

--- scripts/CollisionDetection/Render/Renderable.js
+++ scripts/CollisionDetection/Render/Renderable.js
@@ -2,13 +2,100 @@
 	"use strict";
 
 	App.Renderable = function() {
-		this.initialize = function(painter) {
+		this.painter = null;
+		this.collisionDetector = null;
+		this.canvas = null;
+		this.context = null;
+		this.id = parseInt(Math.random()*100000);
+		this.rendered = null;
+		this.size = {
+			width: 0,
+			height: 0
+		};
+		this.position = {
+			x: 0,
+			y: 0
+		};
+
+		/*
+         * Initializes the object.
+         *
+         * @return void.
+         */
+		this.setupRenderable = function(painter) {
 			console.log('Renderable init');
 			this.painter = painter;
+			this.collisionDetector = this.painter.getCollisionDetector();
+ 			
+ 			this.canvas = document.createElement('canvas');
+			this.context = this.canvas.getContext('2d');
+
+			this.setupEvents();
 		}
 
-		this.render = function() {
+		/*
+         * Sets up our events.
+         *
+         * @return void.
+         */
+		this.setupEvents = function() {
+			this.painter.registerCallback(this.id, this.animateCallback.bind(this));
+		};
 
+		/*
+         * Loads an external image.
+         *
+         * @return void.
+         */
+		this.loadExternalImage = function(imageUrl) {
+			this.imageObj = new Image();
+			this.imageObj.onload = this.onloadOfExternalImage.bind(this)
+			this.imageObj.src = imageUrl;
+		};
+
+		/*
+         * Handles the external image on load.
+         *
+         * @return void.
+         */
+		this.onloadOfExternalImage = function() {
+			this.context.drawImage(this.imageObj,0,0);
+
+			this.size = {
+				width: this.imageObj.width,
+				height: this.imageObj.height
+			};
+
+			this.pixelMap = this.collisionDetector.buildPixelMap(this.canvas);
+			this.rendered = this.canvas;
+		};
+
+		/*
+		 * private function animateCallback()
+		 *
+		 * The callback that the painter runs on fram completion
+		 * so that we can add a new item in the render queue for 
+		 * the painter. The position is updated to the next position
+		 * int the calculatedPositions array.
+		 *
+		 */        
+		this.animateCallback = function(id) {
+			var pixelMap = {
+				pixelMap: this.pixelMap,
+				width: this.size.width,
+				height: this.size.height,
+				x: this.position.x,
+				y: this.position.y
+			};
+
+			this.painter.addToQueue( 
+				this.id,                  // id
+				this.canvas,            // Image
+				this.position.x,  // x position
+				this.position.y,  // y position
+				1,                        // z layer (or collision layer)
+				pixelMap                  // Pixel data (for collision detection)
+			);
 		};
 	}
 })(namespace('CollisionDetection.Render'), jQuery);
