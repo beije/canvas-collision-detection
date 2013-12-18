59c8c2dd4cfe9e0343e555332fe25eb94c43c0ce
beije <git@benjaminhorn.se>
2013-12-18 12:29:32 +0100

    Removed CSS from canvas and set dimensions with attributes instead (in order not to stretch the image)

--- scripts/CollisionDetection/Core.js
+++ scripts/CollisionDetection/Core.js
@@ -4,8 +4,18 @@
 	App.Core = function() {
 		this.painter = null;
 		this.initialize = function(){
+			this.expandCanvas();
 			this.painter = new App.Render.Painter('#main-canvas');
 			this.player = new App.Items.Player(this.painter);
+			this.painter.start();
+		}
+
+		this.expandCanvas = function() {
+			var canvas = $('#main-canvas');
+			var docHeight = $(document.body).innerHeight()-10;
+			var docWidth = $(document.body).innerWidth()-10;
+			canvas.attr('height', docHeight);
+			canvas.attr('width', docWidth);
 		}
 		
 		this.initialize();
